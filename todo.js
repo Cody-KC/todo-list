56bafc072e9098fa918879a5822a8220550f8ddc
Cody-KC <ckokafor1992@gmail.com>
2021-01-03 02:30:36 +0100

    search and filter todos

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 const addTodo = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 
 
 //ADDING TODOS
@@ -22,11 +23,31 @@ addTodo.addEventListener("submit", e=>{
         addTodo.reset();
     };
 
-})
+});
 
 //DELETING TODOS
     list.addEventListener("click", e =>{
         if(e.target.classList.contains("delete")){
             e.target.parentElement.remove();
         }
-    })
+    });
+//KEYUP EVENTS
+const filterTodos = (term) =>{
+    Array.from(list.children)
+    .filter((todo) =>{return !todo.textContent.toLowerCase().includes(term)})
+    .forEach((todo) =>{todo.classList.add("filtered")})
+
+    Array.from(list.children)
+    .filter((todo) =>{return todo.textContent.toLowerCase().includes(term)})
+    .forEach((todo) =>{todo.classList.remove("filtered")})
+
+        // console.log(todo.textContent)
+        // return true;
+    
+};
+
+search.addEventListener("keyup", e =>{
+    const term = search.value.toLowerCase().trim();
+    filterTodos(term);
+})
+
